6b7dd51ef2e37589a5bf16f150a93e356b695b6d
Gabriel Amorim <gabrielo.hca@gmail.com>
2025-12-13 17:59:26 -0300

    exercicio 01

---
+++ S2/A2/exercicios/ex01.py
@@ -0,0 +1,7 @@
+""" solicite ao usuario 3 notas e apresente o resultado da édia aritmética """
+
+nota1 = input('Digite a primeira nota: ')
+nota2 = input('Digite a segunda nota: ')
+nota3 = input('Digite a terceira nota: ')
+media = (float(nota1) + float(nota2) + float(nota3)) / 3
+print(f'A media aritimetica é: {media}')
