df6ab5b85750c52e5f891ed768d34f25afceda5b
Gabriel Amorim <gabrielo.hca@gmail.com>
2026-03-01 02:14:01 -0300

    aula 01

--- classificacao-imagens-DL/src/03-matplotlib/aula01.py
+++ classificacao-imagens-DL/src/03-matplotlib/aula01.py
@@ -1,1 +1,14 @@
-import matplotlib as mpl
+""" Aula 01 - Introdução ao Matplotlib """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(0, 2*np.pi, 500)  # Gera 10 pontos entre 0 e 5
+y = np.cos(4*t)
+
+plt.plot(t, y)
+plt.title("Gráfico de Coseno")
+plt.xlabel("Tempo (s)")
+plt.ylabel("Amplitude")
+# plt.grid(True)
+plt.show()
