bd542ab00c6a405f4339e1270d4bcbc29b526674
Gabriel Amorim <gabrielo.hca@gmail.com>
2026-03-01 02:15:22 -0300

    aula 08

---
+++ classificacao-imagens-DL/src/03-matplotlib/aula08.py
@@ -0,0 +1,20 @@
+""" Aula 08 - Customiizando as escalas dos eixos com Matplotlib """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2*np.pi, 500)
+y = np.cos(3*x)
+
+fig, axe = plt.subplots(figsize=(7, 4))
+axe.plot(x, y)
+axe.set_title("Gráfico de Cosseno", fontsize=16, color="blue")
+axe.set_xlabel("Eixo Tempo", fontsize=12, color="red")
+axe.set_ylabel("Eixo Amplitude", fontsize=12, color="red")
+
+plt.xticks(np.arange(0, 2*np.pi+0.1, np.pi/2),
+           ["0", "π/2", "π", "3π/2", "2π"], color="green")
+plt.yticks(np.arange(-1, 1.1, 0.5), color="green")
+
+axe.grid(True)
+plt.show()
