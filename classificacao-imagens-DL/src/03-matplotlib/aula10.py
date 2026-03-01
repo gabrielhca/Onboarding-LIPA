2802ef5dd19951760e546b96aafaf7d46651aa3d
Gabriel Amorim <gabrielo.hca@gmail.com>
2026-03-01 02:15:51 -0300

    aula 10

---
+++ classificacao-imagens-DL/src/03-matplotlib/aula10.py
@@ -0,0 +1,17 @@
+""" Aula 10 - Identificando com linhas paralelas e marcadores """
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+plt.style.use("ggplot")
+
+x = np.linspace(1, 5, 500)
+y = np.log10(x)
+
+fig, axe = plt.subplots(figsize=(7, 4))
+axe.plot(x, y, lw=1.2, color="green")
+axe.plot([0, 2.5], [0.4, 0.4], lw=1.2, color="gray", ls="--")
+axe.plot([2.5, 2.5], [0, 0.4], lw=1.2, color="gray", ls="--")
+axe.plot([2.5], [0.4], marker="o", markersize=8, color="red")
+axe.set_xticks(np.arange(0, 5.5, 0.5))
+plt.show()
